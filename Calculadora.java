0e48aec4b1cf19d248dbe04a19fa2cd5ce1e8129
Luiz Neto <luizneto901@gmail.com>
2026-04-06 15:05:53 -0300

    Commit da branch oamanhanuncamorre

--- Calculadora.java
+++ Calculadora.java
@@ -2,23 +2,25 @@ public class Calculadora {
 
     /**
      * Executa uma operação matemática básica.
-     * @param operacao
+     * @param operacao Tipo da operação: "divisao"
      * @param a Primeiro número
      * @param b Segundo número
      * @return Resultado da operação
      */
     public double calcular(String operacao, int a, int b) {
         switch (operacao.toLowerCase()) {
-            default:
-                throw new IllegalArgumentException("Operação inválida: " + operacao);
+            case "divisao":
+                if (b == 0) {
+                    throw new IllegalArgumentException("Divisão por zero não é permitida!");
+                }
+                return (double) a / b;
         }
     }
 
     // Método principal para testar
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
-
-        calc.calcular( "", 0, 0 );
+        System.out.println("Divisão: " + calc.calcular("divisao", 10, 5));
     }
 }
 
